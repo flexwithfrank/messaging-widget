e8b3782299dd40862b2d48b4f13a9be95be71d75
videovulture <30363299+videovulture@users.noreply.github.com>
2024-04-07 16:29:11 -0700

    main loader

--- loader.js
+++ loader.js
@@ -2,7 +2,7 @@
 
 const loader = `
 
-<div class="flex items-center justify-center w-full h-full">
+<div class="flex items-center justify-center w-full h-full bg-[#f4f0e6]">
 	<div class="flex justify-center items-center space-x-1 text-xl text-black font-extrabold">
 				<svg fill='none' class="w-6 h-6 animate-spin" viewBox="0 0 32 32" xmlns='http://www.w3.org/2000/svg'>
 					<path clip-rule='evenodd'
